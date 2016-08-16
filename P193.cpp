8ab533a6ebc343557a4f96f9047ffd0d56edf153
Lasse Deleuran <lassedeleuran@gmail.com>
2016-08-16 08:35:01 +0200

    Done P193

--- P193.cpp
+++ P193.cpp
@@ -2,100 +2,52 @@
 #include <stdio.h>
 #include <vector>
 #include <set>
-#include <stack>
-#include <algorithm>
 
-void run(std::set<int> &unset, std::set<int> * const adjacencyList, std::vector<int> &best, std::vector<int> &black) {
-  //std::cerr << "Running, |unset|=" << unset.size() << ", |black|=" << black.size() << ", |best|=" << best.size() << std::endl;
-  // Find smallest number of neighbours:
-  if(unset.size() <= 1) {
-    if(unset.size() == 1) {
-      black.push_back(*unset.begin()); // last is black
-    }
+/*
+best = best coloring of black nodes found so far.
+black = current coloring.
+
+Heuristic algorithm by Brian Fry. It works for the problem statement, but is not a general solution :-( See earlier revision for my own heuristic (coloring nodes with minimal number of neighbours).
+ */
+void run(int node, const int n, std::set<int> * const adjacencyList, std::vector<int> &best, std::vector<int> &black) {
+  if(node == n) {
     if(best.size() < black.size()) {
       best = black;
-    }
-    if(unset.size() == 1) {
-      black.pop_back();
     }
     return;
   }
 
-  unsigned int minNeighbours = 1000;
-  std::vector<int> minSet;
-  for(std::set<int>::const_iterator it = unset.begin(); it != unset.end(); ++it) {
-    if(adjacencyList[*it].size() < minNeighbours) {
-      minSet.clear();
-      minNeighbours = adjacencyList[*it].size();
-      minSet.push_back(*it);
-    }
-    else if(adjacencyList[*it].size() == minNeighbours) {
-      if(black.size() <= 0 || (black.size() + unset.size()) < 15)
-        minSet.push_back(*it);      
-    }
+  bool anyBlackNeighbours = false;
+  for(std::vector<int>::const_iterator it = black.begin(); it != black.end(); ++it) {
+    if(adjacencyList[node].find(*it) != adjacencyList[node].end()) {
+      anyBlackNeighbours = true;
+      break;
+    }      
   }
-  //std::cerr << minSet.size() << " of size " << minNeighbours << std::endl; 
 
-  // iterate through all those nodes:
-  for(std::vector<int>::const_iterator it2 = minSet.begin(); it2 != minSet.end(); ++it2) {
-    const int candidate = *it2;
-    //std::cerr << "Testing " << candidate << std::endl;
-    unset.erase(candidate); // black
-
-    std::set<std::pair<int,int> > rem;
-    std::vector<int> whites;
-    for(std::set<int>::const_iterator it = adjacencyList[candidate].begin(); it != adjacencyList[candidate].end(); ++it) {
-      whites.push_back(*it);
-      unset.erase(*it); // white
-    }
-
-    for(std::vector<int>::const_iterator it = whites.begin(); it != whites.end(); ++it) {
-      for(std::set<int>::const_iterator it3 = adjacencyList[*it].begin(); it3 != adjacencyList[*it].end(); ++it3) {
-	// it3 is adjacent to white
-	adjacencyList[*it3].erase(*it);
-	rem.insert(std::make_pair(*it3, *it));
-      }
-    }
-    //std::cerr << "=> 1 new black, " << whites.size() << " new white, " << rem.size() << "less edges." << std::endl;
-
-    black.push_back(candidate);
-    run(unset, adjacencyList, best, black);
+  if(!anyBlackNeighbours) {
+    black.push_back(node);
+    run(node+1, n, adjacencyList, best, black);
     black.pop_back();
-
-    // Re-insert:
-    for(std::vector<int>::const_iterator it = whites.begin(); it != whites.end(); ++it) {
-      unset.insert(*it);
-    }
-    unset.insert(candidate);
-    for(std::set<std::pair<int,int> >::const_iterator it3 = rem.begin(); it3 != rem.end(); ++it3) {
-      adjacencyList[it3->first].insert(it3->second);
-    }
-  }
+  } 
+  run(node+1, n, adjacencyList, best, black);
 }
 
 /*
-Heuristic: Try to color nodes with min degree black - try all.
-
 k = |edges|
 n = |nodes|
 N = |tests|
-
-black: 0, white: 1, unset: 2
  */
 int main() {
   std::set<int> adjacencyList[100];  
-  std::set<int> unset;  
 
   int N;
   std::cin >> N;
   for(int ignore = 0; ignore < N; ++ignore) {
-    unset.clear();
     int n, k;
     std::cin >> n >> k;
-    for(int i = 0; i < n; ++i) {
+    for(int i = 0; i < n; ++i)
       adjacencyList[i].clear();
-      unset.insert(i);
-    }
     for(int i = 0; i < k; ++i) {
       int a, b;
       std::cin >> a >> b;
@@ -106,10 +58,9 @@ int main() {
     }
 
     std::vector<int> best, black;
-    run(unset, adjacencyList, best, black);
+    run(0, n, adjacencyList, best, black);
 
     std::cout << best.size() << std::endl;
-    std::sort(best.begin(), best.end());
     for(unsigned int i = 0; i < best.size(); ++i) {
       if(i != 0)
 	std::cout << " ";
