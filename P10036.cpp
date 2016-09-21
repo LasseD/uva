d6e2cbf08ed1523e1a5376b5a278c6ac03adfb34
Lasse Deleuran <lassedeleuran@gmail.com>
2016-09-21 22:15:35 +0200

    Done P10036.

--- P10036.cpp
+++ P10036.cpp
@@ -1,49 +1,41 @@
 #include <iostream>
 #include <cstdio>
-
-using namespace std;
+#include <cstring>
 
 int main() {
   int M, N, K, a[10000];
-  bool possible[100], nextPossible[100];
-  cin >> M;
+  int possible[100];
+  scanf("%d", &M);
   for(int cas = 0; cas < M; ++cas) {
     // Read input:
-    cin >> N >> K;
+    scanf("%d %d", &N, &K);
     long sum = 0;
     for(int i = 0; i < N; ++i) {
-      cin >> a[i];
+      scanf("%d", &a[i]);
       if(a[i] < 0)
 	a[i] = -a[i];
       sum += a[i];
     }
-    //cerr << "Sum=" << sum;
     sum %= K;
-    //cerr << ", mod " << K << "=" << sum << endl;
-    for(int i = 0; i < K; ++i) {
-      possible[i] = (sum == i);
-    }
+    memset(possible, 0, K*sizeof(int));
+    possible[sum] = 1;
+
     for(int i = 0; i < N; ++i) {
-      for(int j = 0; j < K; ++j)
-	nextPossible[j] = false;
       // find all divisible:
       int add = 2*K*a[i]-2*a[i];
       for(int j = 0; j < K; ++j) {
-	if(possible[j])
-	  nextPossible[(j+add)%K] = true;
+	int j2 = (j+add)%K;
+	if(possible[j] > 0 && possible[j] <= i+1 && possible[j2] == 0)
+	  possible[j2] = i+2;
       }
-
-      //cerr << "After a[" <<i <<"]=" << a[i] << ": add=" << add << "%K=" << add%K << endl;
-      for(int j = 0; j < K; ++j) {
-	possible[j] = nextPossible[j] || possible[j];
-	//if(possible[j]) cerr << j << " ";
-      }
-      //cerr << endl;
+      if(possible[0] != 0)
+	break;
     }
-    if(possible[0])
-      cout << "Divisible" << endl;
+
+    if(possible[0] != 0)
+      puts("Divisible");
     else
-      cout << "Not divisible" << endl;
+      puts("Not divisible");
   }
   return 0;
 }
