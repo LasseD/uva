ea14e75db87a5f08f5e6dd9bb5146e117e6e199d
ldeleuran <lassedeleuran@gmail.com>
2017-03-02 11:29:29 +0100

    Added P11151.

--- P11151.cpp
+++ P11151.cpp
@@ -1,60 +1,43 @@
-#include <iostream>
-#include <stdio.h>
-#include <algorithm>
-#include <cstring>
-
-#define LINE_LENGTH 1008
-
-#define MAX(a,b) ((a) < (b) ? (b) : (a))
-
-int longestSubSequence(char const * const s1, char const * const s2, int len) {
-  if(len == 0)
-    return 0;
-  int *a = new int[2*(len+1)]; // +1 for empty first character.
-  int prevColumn = 0;
-  for(int i = 0; i <= len; ++i)
-    a[i] = 0;
-  for(int s2I = 0; s2I < len; ++s2I) {
-    int startI = (1-prevColumn)*(len+1);
-    int startPrevI = prevColumn*(len+1);
-    a[startI] = 0; // empty first character.
-    for(int s1I = 0; s1I < len; ++s1I) {
-      if(s1[s1I] == s2[s2I]) {
-	a[startI+s1I+1] = a[startPrevI+s1I] + 1;
-      }
-      else {
-	int idxA = startI+s1I;
-	int idxB = startPrevI+s1I+1;
-	a[startI+s1I+1] = MAX(a[idxA], a[idxB]);
-      }
-      //std::cerr << a[startI+s1I+1] << " ";
-    }
-    prevColumn = 1-prevColumn;
-    //std::cerr << std::endl;
-  }
-
-  int ret = a[prevColumn*(len+1)+len];
-  delete[] a;
-  return ret;
-}
-
 int main() {
-  char *line1 = new char[LINE_LENGTH];
-  char *line2 = new char[LINE_LENGTH];
-
+  int A[3300];
+  char s[1009], cs;
+  int *pp, *p, *c;
   int cases;
-  scanf("%d\n", &cases);
-
+  scanf("%d", &cases); getchar();
   for(int cas = 0; cas < cases; ++cas) {
-    gets(line1);
-    int size = strlen(line1);
-    std::copy(line1, line1+size, line2);
-    std::reverse(line1, line1+size);
-    line1[size] = '\0';
-    line2[size] = '\0';
-    //std::cerr << line1 << " VS " << line2 << ", len=" << size << std::endl;
-    
-    std::cout << longestSubSequence(line1, line2, size) << std::endl;    
+    int N = 0;
+    while(isalpha(cs = getchar()))
+      s[N++] = cs;
+    if(N <= 1) {
+      cout << N << endl;
+      continue;
+    }
+
+    FORI(N)
+      A[i] = 0;
+    FORI(N)
+      A[i+N] = 1;
+    pp = &A[0];
+    p = &A[N];
+    c = &A[2*N];
+    for(int len = 2; len <= N; ++len) {
+      //cerr << "len=" << len << ":" << endl;
+      FORI(N-len+1) {
+	if(s[i] == s[i+len-1]) {
+	  c[i] = 2+pp[i+1];
+	}
+	else {
+	  c[i] = MAX(p[i], p[i+1]);
+	}
+	//cerr << " " << s.substr(i, len) << ": " << c[i] << endl;
+      }
+      // TODO: Update p, pp and c;
+      int *tmp = pp;
+      pp = p;
+      p = c;
+      c = tmp;
+    }
+    cout << p[0] << endl;
   }
   return 0;
 }
