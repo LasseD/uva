d9933c1525716979ee93cc45e4ab41bf069826c9
ldeleuran <lassedeleuran@gmail.com>
2017-06-30 20:58:22 +0200

    Added P10702.

--- P10702.cpp
+++ P10702.cpp
@@ -7,8 +7,13 @@ Stop early by sorting and using "highestProfitGoingToCity" and "sortedCities".
 
 Attempt 2: 
 Only update profit(key) in "sortedCities" => less "sorting" in each step.
+=> No improvement :(
+
+Attempt 3:
+Use sorted in-edges (Code block 2 below)
 
  */
+/*
 int main() {
   int C, S, E, T, city;
   int profitMatrix[100][100], profit[100], tmpProfit[100], highestProfitGoingToCity[100];
@@ -53,6 +58,67 @@ int main() {
 
       FORJ(C) {
 	sortedCities[j].first = profit[sortedCities[j].second];
+      }
+    } // FORK(T) Walk T times
+
+    int bestProfit = 0;
+    FORI(E) {
+      scanf("%d", &city);
+      --city;
+      if(profit[city] > bestProfit)
+	bestProfit = profit[city];
+    }
+    printf("%d\n", bestProfit);
+  }
+}
+*/
+int main() {
+  int C, S, E, T, city;
+  int profitMatrix[100][100], profit[100], tmpProfit[100];
+  PI sortedInEdges[100][100]; // value -> from city
+
+  while(true) {
+    scanf("%d %d %d %d", &C, &S, &E, &T);
+    if(0 == (C | S | E | T))
+      return 0;
+    --S;
+
+    // C cities. S start. E ends. T visiting.
+    FORI(C) {
+      FORJ(C) {
+	scanf("%d", &profitMatrix[j][i]);
+	if(i == S) {
+	  profit[j] = profitMatrix[j][i]; // Note: Becomes 0 for S.
+	}
+	sortedInEdges[j][i] = PI(-profitMatrix[j][i], i); // '-' to reverse sort.
+      }
+    }
+    --T;
+
+    // Perform sortings:
+    FORJ(C) {
+      sort(sortedInEdges[j], sortedInEdges[j]+C);
+    }
+
+    // Compute profit:
+    int maxProfit = 0;
+    FORK(T) { // Walk T times:
+      FORJ(C) {
+	if(profit[j] > maxProfit)
+	  maxProfit = profit[j];
+      } // FORJ
+
+      FORJ(C) { // Check each city
+	tmpProfit[j] = 0;
+	for(int xi = 0; xi < C && tmpProfit[j] < maxProfit - sortedInEdges[j][xi].first; ++xi) {
+	  int i = sortedInEdges[j][xi].second;
+	  int tryProfit = profit[i] - sortedInEdges[j][xi].first;
+	  if(tryProfit > tmpProfit[j])
+	    tmpProfit[j] = tryProfit;
+	} // for xi
+      } // FORJ
+      FORJ(C) { // Update each city
+	profit[j] = tmpProfit[j];
       }
     } // FORK(T) Walk T times
 
