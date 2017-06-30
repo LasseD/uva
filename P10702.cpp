5da5415fec85d0a9eafdc293c5c40d355707688b
ldeleuran <lassedeleuran@gmail.com>
2017-06-30 15:53:39 +0200

    Added P10702.

--- P10702.cpp
+++ P10702.cpp
@@ -1,43 +1,68 @@
+/*
+OK. Let's try to improve this: Improce the computation of tmpProfit.
+
+Attempt 1:
+Stop early by sorting and using "highestProfitGoingToCity" and "sortedCities".
+=> Time from 0.030 -> 0.010 (Only 6 others here. None at 0.000).
+
+Attempt 2: 
+Only update profit(key) in "sortedCities" => less "sorting" in each step.
+
+ */
 int main() {
   int C, S, E, T, city;
-  LL profitMatrix[10000], profit[100], tmpProfit[100];
+  int profitMatrix[100][100], profit[100], tmpProfit[100], highestProfitGoingToCity[100];
+  PI sortedCities[100]; // value -> city
 
   while(true) {
-    cin >> C >> S >> E >> T;
+    scanf("%d %d %d %d", &C, &S, &E, &T);
     if(0 == (C | S | E | T))
       return 0;
     --S;
+    FORI(C) {
+      sortedCities[i] = PI(-1000000000, i);
+      highestProfitGoingToCity[i] = 0;
+    }
+    sortedCities[S] = PI(0, S);
+
     // C cities. S start. E ends. T visiting.
     FORI(C) {
-      profit[i] = -1; // Indicating no profit.
+      profit[i] = -1000000000; // Indicating no profit.
       FORJ(C) {
-	cin >> profitMatrix[i * 100 + j];
+	scanf("%d", &profitMatrix[j][i]);
+	if(profitMatrix[j][i] > highestProfitGoingToCity[j])
+	  highestProfitGoingToCity[j] = profitMatrix[j][i];
       }
     }
     profit[S] = 0;
 
     // Compute profit:
     FORK(T) { // Walk T times:
+      sort(sortedCities, sortedCities+C);
+
       FORJ(C) { // Update each city
 	tmpProfit[j] = 0;
-	FORI(C) {
-	  if(profit[i] == -1)
-	    continue;
-	  int tryProfit = profit[i] + profitMatrix[i * 100 + j];
-	  tmpProfit[j] = MAX(tmpProfit[j], tryProfit);
+	for(int xi = C-1; xi >= 0 && tmpProfit[j] < sortedCities[xi].first + highestProfitGoingToCity[j]; --xi) {
+	  int i = sortedCities[xi].second;
+	  int tryProfit = profit[i] + profitMatrix[j][i];
+	  if(tryProfit > tmpProfit[j])
+	    tmpProfit[j] = tryProfit;
 	}
       }
+      swap(profit, tmpProfit);
+
       FORJ(C) {
-	profit[j] = tmpProfit[j];
+	sortedCities[j].first = profit[sortedCities[j].second];
       }
-    } // FORK(T) Walk T times.
+    } // FORK(T) Walk T times
 
     int bestProfit = 0;
     FORI(E) {
-      cin >> city; --city;
+      scanf("%d", &city);
+      --city;
       if(profit[city] > bestProfit)
 	bestProfit = profit[city];
     }
-    cout << bestProfit << endl;
+    printf("%d\n", bestProfit);
   }
 }
