c862231c3f63b5e214f2b31ece75d9f807fcff42
ldeleuran <lassedeleuran@gmail.com>
2017-07-25 14:32:12 +0200

    Now able to read user_data.csv

---
+++ oxl/oxl.cpp
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+
+#define ORIGIN_BROWSE_SEARCH 0
+#define ORIGIN_BROWSE 1
+#define ORIGIN_SEARCH 2
+#define ORIGIN_NOTIFICATION_CENTER 3
+#define ORIGIN_HOME 4
+#define ORIGIN_PUSH 5
+#define ORIGIN_DRAWER 6
+#define ORIGIN_UNKNOWN 7
+#define ORIGIN_DEEPLINK 8
+
+using namespace std;
+
+typedef pair<double,double> PD;
+
+struct Impression {
+  long time;
+  bool isFirstMessage;
+  // Ignore device (don't have time for this right now).
+  int origin; // See ORIGIN_* enum ^^
+  int adID;
+  // Ignore image count for now.
+  int interactions; // Sum impressions, views and messages (No time for better analysis for now)
+
+  Impression() : time(-1), isFirstMessage(false), origin(-1), adID(-1), interactions(-1) {}
+  Impression(const Impression &o) : time(o.time), isFirstMessage(o.isFirstMessage), origin(o.origin), adID(o.adID), interactions(o.interactions) {}
+  Impression(long time, bool isFirstMessage, int origin, int adID, int interactions) : time(time), isFirstMessage(isFirstMessage), origin(origin), adID(adID), interactions(interactions) {}
+};
+
+struct User {
+  PD lastPosition;
+  vector<Impression> impressions;
+};
+
+void readUserDataCsv(map<int,User*> &userMap) {
+  string line;
+
+  ifstream file;
+  file.open("user_data.csv");
+
+  getline(file, line); // Header
+  while(getline(file, line, ',')) {
+    // Read time:
+    // Timestamp of type "2017-06-30 12:34:56"
+    //                   0 2 4 6 8  12  16  18
+    int month, day, hour, minute, second;
+    sscanf(&(line.c_str()[6]), "%d-%d %d:%d:%d", &month, &day, &hour, &minute, &second);
+
+    long time = month * 32 + day;
+    time = 24*time + hour;
+    time = 60*time + minute;
+    time = 60*time + second;
+    //cerr << "Read time " << month <<", "<< day<<", "<< hour<<", "<< minute<<", "<< second << endl;
+    // Read user id:
+    getline(file, line, ',');
+    int id;
+    sscanf(&(line.c_str()[1]), "%d", &id);
+    //cerr << "User id " << id << endl;
+    // event:
+    getline(file, line, ',');
+    bool isFirstMessage = line[1] == 'f';
+    //cerr << "First message: " << isFirstMessage << endl;
+    // Channel (ignore for now:
+    getline(file, line, ',');
+    // User position:
+    double lat, lon;
+    getline(file, line, ',');
+    sscanf(&(line.c_str()[1]), "%lf", &lat);    
+    getline(file, line, ',');
+    sscanf(&(line.c_str()[1]), "%lf", &lon);    
+    //cerr << "Position: " << lat << ", " << lon << endl;
+
+    // Origin:
+    getline(file, line, ',');
+    int origin;
+    if(line == "\"browse\"")
+      origin = ORIGIN_BROWSE;
+    else if(line == "\"browse_search\"")
+      origin = ORIGIN_BROWSE_SEARCH;
+    else if(line == "\"home\"")
+      origin = ORIGIN_HOME;
+    else if(line == "\"notification_center\"")
+      origin = ORIGIN_NOTIFICATION_CENTER;
+    else if(line == "\"search\"")
+      origin = ORIGIN_SEARCH;
+    else if(line == "\"push\"")
+      origin = ORIGIN_PUSH;
+    else if(line == "\"drawer\"")
+      origin = ORIGIN_DRAWER;
+    else if(line == "\"\"")
+      origin = ORIGIN_UNKNOWN;
+    else if(line == "\"deeplink\"")
+      origin = ORIGIN_DEEPLINK;
+    else
+      cerr << "Unknown origin: " << line << endl;
+    
+    // Rest of line:
+    int adID;
+    float images, impressions, views, messages;
+    getline(file, line, ',');
+    sscanf(&(line.c_str()[1]), "%d", &adID);    
+    getline(file, line, ',');
+    sscanf(&(line.c_str()[1]), "%f", &images);    
+    getline(file, line, ',');
+    sscanf(&(line.c_str()[1]), "%f", &impressions);    
+    getline(file, line, ',');
+    sscanf(&(line.c_str()[1]), "%f", &views);    
+    getline(file, line);
+    sscanf(&(line.c_str()[1]), "%f", &messages);    
+    //cerr << "Rest: " << adId <<", "<< images<<", "<< impressions<<", "<< views<<", "<< messages << endl;
+
+    User *user;
+    if(userMap.find(id) == userMap.end()) {
+      user = new User;
+      userMap[id] = user;
+    }
+    else
+      user = userMap[id];
+    user->lastPosition = PD(lat,lon);
+
+    Impression impression(time, isFirstMessage, origin, adID, impressions+views+messages);
+
+    user->impressions.push_back(impression);
+  }
+}
+
+int main() {
+  map<int,User*> userMap;
+  // Read user_data.csv for user map.
+  readUserDataCsv(userMap);
+}
