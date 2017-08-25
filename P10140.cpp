5a2e1a36fd1582bbd4e92db922317ccbb8eebdd6
ldeleuran <lassedeleuran@gmail.com>
2017-08-25 10:35:16 +0200

    Added P10140.

--- P10140.cpp
+++ P10140.cpp
@@ -1,103 +1,99 @@
-#include <iostream>
+#include <bitset>
 
-#define PRIME_LEN 500000
+#define PRIME_LEN 50000
+#define PRIME_LEN_HIGH 1000002
 
 class PrimeHandler {
-  bool primes[PRIME_LEN];
+  std::bitset<PRIME_LEN> primes;
+  std::bitset<PRIME_LEN_HIGH> highPrimes;
 
 public:
-  PrimeHandler() {
-    for(int i = 0; i < PRIME_LEN; ++i)
-      primes[i] = true;
+  void init() {
+    primes.set();
     // Sieve primes:
-    for(int i = 0; i*i < PRIME_LEN; ++i) {
+    for(long i = 0; i*i < PRIME_LEN; ++i) {
       if(!primes[i])
 	continue;
       // Mark all uneven multiples as non-prime:
-      int basePrime = 1+2*(i+1);
-      for(int multiple = 3; true; multiple += 2) {
-	int notAPrime = basePrime*multiple;
-	int notAPrimeI = notAPrime/2-1;
+      long basePrime = 1+2*(i+1);
+      for(long multiple = 3; true; multiple += 2) {
+	long notAPrime = basePrime*multiple;
+	long notAPrimeI = notAPrime/2-1;
 	if(notAPrimeI >= PRIME_LEN)
 	  break;
-	primes[notAPrimeI] = false;
+	primes.set(notAPrimeI, false);
       }
     }
   }
-  
-  bool isPrime(long n) {
+
+  long nextPrimeLow(long n) const {
     if(n == 2)
-      return true;
-    if(n < 2 || (n%2==0))
-      return false;
-    if(n/2-1 < PRIME_LEN)
-      return primes[n/2-1];
-
-    // 3+:
-    int prime = 3;
-    while(sumDigitsFactors <= sumDigitsN && prime*prime <= n) {
-      if(n % prime == 0)
-	return false;
-      prime = nextPrime(prime);
-    }
-    return true;
-  }
-
-  long nextPrime(long n) const {
+      return 3; // Special case.
     long ni = n/2;
-    bool ok = true;
+    if(ni >= PRIME_LEN)
+      dieSlowly();
+
     while(!primes[ni])  {
-      if(ni >= PRIME_LEN) {
-	ok = false;
-	break;
-      }
       ++ni;      
     }
-    if(ok)
-      return 1+(ni+1)*2;
-    
-    //std::cerr << "Returning next prime(" << n << "): " << (1+(ni+1)*2) << std::endl;
-    if(n % 2 == 0)
-      ++n; // Only check uneven numbers.
-    while(!isPrime(n))
-      n += 2;
-    return n;
+    return 1+(ni+1)*2;
   }
-};
+  
+  void go(long L, long U) {
+    if(L < 0 || U < 0 || L > U || U > 2147483647 || U-L > PRIME_LEN_HIGH)
+      dieSlowly();
 
-int main() {
-  PrimeHandler ph;
-  long L, U;
+    //cerr << "L=" << L << ", U=" << U << endl;
+    highPrimes.set();
+    // Sieve primes:
+    long prime = 2;
+    while(prime*prime <= U) {
+      long mult = (L+prime-1)/prime;
+      if(mult < 2)
+	mult = 2;
+      for(; mult*prime <= U; ++mult) {
+	highPrimes.set(mult*prime-L, false);
+      }
+      prime = nextPrimeLow(prime);
+    }
 
-  while(cin >> L >> U) {
-    bool closeDone = false, distDone = false;
-    L = ph.nextPrime(L-1);
     long closeL = L, distL = L, closeU = -1, distU = -1;
-    long R = L;
-    int step = 0;
-    while(!(closeDone && distDone)) {
-      ++steps;
-      long RR = ph.nextPrime(R);
-      if(RR > U)
-	break;
+    // Compute longervals:
+    long R = -1;
+    for(long RR = L; RR <= U; ++RR) {
+      if(!highPrimes[RR-L])
+	continue; // not a prime
 
-      if(closeU == -1 || (closeU-closeL)>(RR-R)) {
+      if(R != -1 && (closeU == -1 || (closeU-closeL)>(RR-R))) {
 	closeL = R;
 	closeU = RR;
-	if(closeU-closeL == 2)
-	  closeDone = true;
       }
-      if(distU == -1 || (distU-distL)<(RR-R)) {
+      if(R != -1 && (distU == -1 || (distU-distL)<(RR-R))) {
 	distL = R;
 	distU = RR;
       }
       R = RR;
     }
-    cerr << "Steps: " << steps << endl;
+
     if(closeU == -1)
       cout << "There are no adjacent primes." << endl;
     else
       cout << closeL << "," << closeU << " are closest, " << distL << "," << distU << " are most distant." << endl;
+
+  }
+};
+
+int main() {
+  PrimeHandler ph;
+  ph.init();
+  long L, U;
+
+  while(cin >> L >> U) {
+    if(L < 2)
+      L = 2;
+    if(U < L)
+      U = L;
+    ph.go(L, U);
   }
   return 0;
 }
