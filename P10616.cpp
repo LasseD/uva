b157b0dd7fb3c493c6a324208c68aa0adda6042c
Lasse Deleuran <lassedeleuran@gmail.com>
2017-10-19 23:25:49 +0200

    Added P10616.

---
+++ P10616.cpp
@@ -0,0 +1,16 @@
+int main() {
+  int N, Q, DP[20][200][10], a[200], D, M;
+  for(int cas = 1; true; ++cas) {
+    cin >> N >> Q;
+    if(N == 0 && Q == 0)
+      return 0;
+    cout << "SET " << cas << ":" << endl;
+    FORI(N)
+      cin >> a[i];
+    FORI(Q) {
+      cin >> D >> M;
+      
+      // TODO: Compute ret!
+    }
+  }
+}
